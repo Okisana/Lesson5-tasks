8d33f06b558414adcf0aea7085eec4296d9f6b09
Oksana <77448138+Okisana@users.noreply.github.com>
2021-05-20 17:18:38 +0300

    2nd way

---
+++ atmTask2.js
@@ -0,0 +1,43 @@
+// 2. ATM
+// Asks for action "Please tell me what to do": + | -
+// Asks for amount "How much? "
+// Print out current ballance "Current ballance is: 100"
+import { accessSync, readFileSync, writeFileSync } from "fs";
+import readline from "readline";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+try {
+  const filePath = `${process.cwd()}/atmBalance.json`;
+  accessSync(filePath);
+  const currentMoney = readFileSync(filePath, "utf8");
+  let currentMoneyDecoded = JSON.parse(currentMoney);
+  console.log(`Your balance is: ${currentMoneyDecoded.balance}`);
+
+  rl.question("Enter action + or - : ", (action) => {
+    if (action !== "+" && action !== "-") {
+      console.log(`Please choose correct action: + or -`);
+      rl.close();
+      // return;
+    } else {
+      rl.question("Enter amount of money: ", (moneyAmount) => {
+        if (action === "+") {
+          currentMoneyDecoded.balance += parseInt(moneyAmount);
+        } else {
+          currentMoneyDecoded.balance -= parseInt(moneyAmount);
+        }
+        console.log(
+          `Current account balance is: ${currentMoneyDecoded.balance}`
+        );
+        writeFileSync(filePath, JSON.stringify(currentMoneyDecoded));
+        rl.close();
+      });
+    }
+  });
+} catch (error) {
+  console.error("There was an error: ", error);
+  rl.close();
+}
